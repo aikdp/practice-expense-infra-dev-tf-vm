477328f28d829309ea6a8a345392b6f014d48e1f
aikdp <kdprasad028@gmail.com>
2025-03-05 23:24:43 +0530

    VPC-to-CDN VM with TF practice

---
+++ 03-bastion/main copy.tf
@@ -0,0 +1,16 @@
+#Instance
+module "bastion" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = local.ami_id
+  name = local.resource_name
+
+  instance_type          = "t3.micro"
+ 
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id              = local.public_subnet_id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
