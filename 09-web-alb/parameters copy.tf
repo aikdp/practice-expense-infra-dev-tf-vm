36a28c17a4fde50d6657f3931076173981756348
aikdp <kdprasad028@gmail.com>
2025-03-07 18:45:46 +0530

    TF VPC-CDN practice

--- 09-web-alb/parameters copy.tf
+++
@@ -1,8 +0,0 @@
-
-
-resource "aws_ssm_parameter" "app_alb_target_group" {
-  name        = "/${var.project}/${var.environment}/app_alb_target_group"
-  description = "The parameter description aws_lb_listener"
-  type        = "String"
-  value       = aws_lb_target_group.backend.arn
-}
