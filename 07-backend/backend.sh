de493ff1bc63880757167ade64a5ab05d1ee54fe
aikdp <kdprasad028@gmail.com>
2025-03-07 19:23:40 +0530

    TF VPC-CDN practice

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -2,7 +2,7 @@
 comp=$1
 env=$2
 echo "Component is:$comp, Environment is: $env"
-sudo dnf install ansible -y
+sudo dnf install -y ansible
 ansible-pull -i localhost, -U https://github.com/aikdp/expense-ansible-tf-roles.git main.yaml -e component=$comp -e environment=$env
 #passing terraform variables to shell, shell to ansible
 #in command line, we are passing to ansible playbook variables
